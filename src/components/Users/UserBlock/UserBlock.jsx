b8de124df2635dc65be853e7dc0ee0bade413e3e
Hurricane0 <nikitadv777@gmail.com>
2019-12-22 23:01:42 +0200

    Testing Class Component

--- src/components/Users/UserBlock/UserBlock.jsx
+++ src/components/Users/UserBlock/UserBlock.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./UserBlock.module.css";
+import userAvatar from "../../../assets/images/userAvatar.jpg";
 
 let UserBlock = props => {
   let unfollow = () => {
@@ -8,33 +9,43 @@ let UserBlock = props => {
   let follow = () => {
     props.follow(props.user.id);
   };
+  let followingButtonStyle = {
+    backgroundColor: "#04D91B",
+    boxShadow: "inset 0px -2px 1px #03a013, inset 0px 2px 1px #03a013"
+  };
+  let followButtonStyle = {
+    backgroundColor: "#fec92d",
+    boxShadow: "0px 2px 0px #cea321"
+  };
   return (
     <div className={s.wrapper}>
       <div className={s.left_block}>
-        <img className={s.avatar} src={props.user.avatarUrl} alt="UserAvatar" />
+        <img className={s.avatar} src={userAvatar} alt="UserAvatar" />
         {props.user.followed ? (
           <button
             key={props.user.id}
             onClick={unfollow}
             className={s.follow_button}
+            style={followingButtonStyle}
           >
-            Unfollow
+            Following
           </button>
         ) : (
           <button
             key={props.user.id}
             onClick={follow}
             className={s.follow_button}
+            style={followButtonStyle}
           >
             Follow
           </button>
         )}
       </div>
       <div className={s.right_block}>
-        <span className={s.username}>{props.user.username}</span>
+        <span className={s.username}>{props.user.name}</span>
         <span className={s.status}>{props.user.status}</span>
-        <span className={s.country}>{props.user.location.country}</span>
-        <span className={s.city}>{props.user.location.city}</span>
+        <span className={s.country}>{"props.user.location.country"}</span>
+        <span className={s.city}>{"props.user.location.city"}</span>
       </div>
     </div>
   );
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import UserBlock from "./UserBlock/UserBlock";
+import * as axios from "axios";
 
-let Users = props => {
-  return (
-    <div>
-      {props.users.map(user => (
-        <div key={user.id}>
-          <UserBlock
-            user={user}
-            follow={props.follow}
-            unfollow={props.unfollow}
-          />
-        </div>
-      ))}
-    </div>
-  );
-};
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios("http://localhost:3000/items").then(response => {
+      this.props.setUsers(response.data);
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.props.users.map(user => (
+          <div key={user.id}>
+            <UserBlock
+              user={user}
+              follow={this.props.follow}
+              unfollow={this.props.unfollow}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
